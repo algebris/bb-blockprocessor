02e65f65951c0b18684bbd9f353b767513a97399
Vyacheslav Frolov <algebris@gmail.com>
2018-05-06 19:50:30 +0700

    tiny fix for logs

--- utils/logging.js
+++ utils/logging.js
@@ -2,13 +2,7 @@ const _ = require('lodash');
 const bunyan = require('bunyan');
 
 const conf = {
-  streams: [{
-    path: 'app.log'
-  },
-  {
-    stream: process.stdout
-  }
-]
+  streams: [{ stream: process.stdout }]
 };
 
 module.exports = opts => bunyan.createLogger(_.assign(opts, conf));
